0cdb9864d1805e8cb30dbfbaf4205a9c6830aa54
clemiblac <48965151+clemiblac@users.noreply.github.com>
2020-07-08 11:00:04 -0400

    table built and data inserted

--- Mini_Assignments/08-HW_HTML_Table/Unsolved/static/js/app.js
+++ Mini_Assignments/08-HW_HTML_Table/Unsolved/static/js/app.js
@@ -1,16 +1,27 @@
 // Use D3 to select the table
+var studenttable=d3.select("#student-table");
 
 // Use D3 to select the table body
+var tbody=d3.select("#student-tbody");
 
 // Use D3 to set the table class to `table table-striped`
-
+var tablestyle=d3.select(".grades").attr("class","table table-striped")
 // Use a forEach function to loop through the array of objects (from your data)
 grades.forEach((studentgrade) => {
+  console.log(studentgrade);
+  
+  // Append one table row per student/grade
+  var row = tbody.append("tr");
 
   // Assign values to the `student` name variable and the `grade` variable
+  Object.entries(studentgrade).forEach(function([key, value]) {
+    console.log(key, value);
 
-  // Append one table row per student/grade
+
 
   // append one cell for the student and one cell for the grade
+  var cell = row.append("td");
+  cell.text(value);
 
+  });
 });
