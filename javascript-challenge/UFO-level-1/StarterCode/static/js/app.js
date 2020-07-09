fd1338acf85fc3926f8ff01d8dc3c7cb4f013308
clemiblac <48965151+clemiblac@users.noreply.github.com>
2020-07-09 15:19:48 -0400

    next step event creation for date time column

--- javascript-challenge/UFO-level-1/StarterCode/static/js/app.js
+++ javascript-challenge/UFO-level-1/StarterCode/static/js/app.js
@@ -28,14 +28,3 @@ tableData.forEach((ufo) => {
 
 });
 
-//$(function() {
-  //$('tr > td:odd').each(function(index) {
-      //var scale = [ ['table-danger', 60], ['table-warning', 70]];
-      //var score = $(this).text();
-      //for (var i = 0; i < scale.length; i++) {
-          //if (score <= scale[i][1]) {
-              //$(this).addClass(scale[i][0]);
-         // }
-     // }
- // });
-//});
